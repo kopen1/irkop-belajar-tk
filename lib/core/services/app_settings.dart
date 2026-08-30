e090e6e2bcffaf6d407ed75de7a978c1a28c63f7
Kopen <89531508+kopen1@users.noreply.github.com>
2026-08-31 02:51:46 +0700

    feat: tambah pengaturan aplikasi

---
+++ lib/core/services/app_settings.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/foundation.dart';
+
+class AppSettings {
+  AppSettings._();
+  static final AppSettings instance = AppSettings._();
+
+  final ValueNotifier<bool> effectsEnabled = ValueNotifier<bool>(true);
+  final ValueNotifier<bool> narrationEnabled = ValueNotifier<bool>(true);
+  final ValueNotifier<bool> randomQuiz = ValueNotifier<bool>(true);
+  final ValueNotifier<bool> fullscreen = ValueNotifier<bool>(false);
+  final ValueNotifier<double> musicVolume = ValueNotifier<double>(0.65);
+  final ValueNotifier<double> effectsVolume = ValueNotifier<double>(1.0);
+  final ValueNotifier<int> totalScore = ValueNotifier<int>(0);
+  final ValueNotifier<int> totalCorrect = ValueNotifier<int>(0);
+  final ValueNotifier<int> activitiesPlayed = ValueNotifier<int>(0);
+
+  void resetProgress() {
+    totalScore.value = 0;
+    totalCorrect.value = 0;
+    activitiesPlayed.value = 0;
+  }
+}
