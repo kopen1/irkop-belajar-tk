47db570ffed177cb34882b1c626c7ddf7bf723aa
Kopen <89531508+kopen1@users.noreply.github.com>
2026-08-30 21:20:27 +0700

    fix: replace removed dart html AudioContext API

--- lib/services/background_music_web.dart
+++ lib/services/background_music_web.dart
@@ -1,12 +1,13 @@
 import 'dart:async';
-import 'dart:html' as html;
+
+import 'package:web/web.dart' as web;
 
 class BackgroundMusic {
   BackgroundMusic._();
 
   static final BackgroundMusic instance = BackgroundMusic._();
 
-  html.AudioContext? _context;
+  web.AudioContext? _context;
   Timer? _timer;
   var _index = 0;
 
@@ -24,13 +25,7 @@ class BackgroundMusic {
   void start() {
     if (_timer != null) return;
 
-    _context ??= html.AudioContext();
-    final context = _context!;
-
-    if (context.state == 'suspended') {
-      context.resume();
-    }
-
+    _context ??= web.AudioContext();
     _playNext();
     _timer = Timer.periodic(
       const Duration(milliseconds: 420),
@@ -50,17 +45,12 @@ class BackgroundMusic {
     final oscillator = context.createOscillator();
     final gain = context.createGain();
 
-    oscillator
-      ..type = 'sine'
-      ..frequency.value = _notes[_index];
-
+    oscillator.frequency.value = _notes[_index];
     gain.gain.value = .045;
 
-    oscillator
-      ..connectNode(gain)
-      ..start();
-
-    gain.connectNode(context.destination);
+    oscillator.connect(gain);
+    gain.connect(context.destination);
+    oscillator.start();
 
     Future<void>.delayed(const Duration(milliseconds: 330), () {
       oscillator.stop();
