270d5b27e37ff19f58a18992a4c63e91e15fe8ec
Kopen <89531508+kopen1@users.noreply.github.com>
2026-08-30 20:55:01 +0700

    feat: open manual play intro instead of timed splash

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'features/splash/splash_page.dart';
+import 'features/intro/play_intro_page.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -9,7 +9,7 @@ void main() {
     MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Belajar TK',
-      home: const SplashPage(),
+      home: const PlayIntroPage(),
     ),
   );
 }
