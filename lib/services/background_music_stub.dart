ab588fdfe3b60049482566b50c14689493ae1653
Kopen <89531508+kopen1@users.noreply.github.com>
2026-08-30 21:39:29 +0700

    feat: keep music controls available on all platforms

--- lib/services/background_music_stub.dart
+++ lib/services/background_music_stub.dart
@@ -1,9 +1,17 @@
+import 'package:flutter/foundation.dart';
+
 class BackgroundMusic {
   BackgroundMusic._();
 
   static final BackgroundMusic instance = BackgroundMusic._();
 
+  final ValueNotifier<bool> enabled = ValueNotifier<bool>(false);
+
   void start() {}
 
   void stop() {}
+
+  void toggle() {
+    enabled.value = !enabled.value;
+  }
 }
