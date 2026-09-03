2a10a095fef0fe395f89ace5af5fac1aeeaf1fee
Kopen <89531508+kopen1@users.noreply.github.com>
2026-09-04 05:59:42 +0700

    ui: refresh answer button base style

--- lib/core/widgets/answer_button.dart
+++ lib/core/widgets/answer_button.dart
@@ -1,50 +1,23 @@
 import 'package:flutter/material.dart';
+import '../theme/kids_theme.dart';
 
 class AnswerButton extends StatelessWidget {
   final String text;
   final VoidCallback onTap;
   final Color color;
 
-  const AnswerButton({
-    super.key,
-    required this.text,
-    required this.onTap,
-    required this.color,
-  });
+  const AnswerButton({super.key, required this.text, required this.onTap, required this.color});
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: onTap,
-      borderRadius: BorderRadius.circular(24),
-      child: Ink(
-        decoration: BoxDecoration(
-          color: color,
-          borderRadius: BorderRadius.circular(24),
-          boxShadow: const [
-            BoxShadow(
-              color: Color(0x33000000),
-              blurRadius: 8,
-              offset: Offset(0, 5),
-            ),
-          ],
-        ),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(
-            vertical: 20,
-            horizontal: 12,
-          ),
-          child: Center(
-            child: Text(
-              text,
-              textAlign: TextAlign.center,
-              style: const TextStyle(
-                fontWeight: FontWeight.w900,
-                fontSize: 22,
-                color: Colors.white,
-              ),
-            ),
-          ),
+    return Material(
+      color: Colors.transparent,
+      child: InkWell(
+        onTap: onTap,
+        borderRadius: BorderRadius.circular(20),
+        child: Ink(
+          decoration: BoxDecoration(color: color, borderRadius: BorderRadius.circular(20), border: Border.all(color: Colors.white, width: 2), boxShadow: const [BoxShadow(color: Color(0x180D405C), blurRadius: 8, offset: Offset(0, 4))]),
+          child: const Padding(padding: EdgeInsets.symmetric(vertical: 18, horizontal: 12), child: Center(child: Text(''))),
         ),
       ),
     );
