9ae50abb663da76cbcd4afa5ff4d178bddb6ad05
Kopen <89531508+kopen1@users.noreply.github.com>
2026-09-05 03:33:39 +0700

    feat(rive): initialize Rive runtime on all platforms

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:rive/rive.dart' as rive;
 
 import 'core/theme/kids_theme.dart';
 import 'core/widgets/web_mobile_shell.dart';
 import 'features/home/home_page.dart';
 import 'services/background_music.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await rive.RiveNative.init();
   BackgroundMusic.instance.start();
 
   runApp(
