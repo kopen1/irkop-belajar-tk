1cf6b65b82b4115be34de1c9a8713f7a55ee1936
Kopen <89531508+kopen1@users.noreply.github.com>
2026-09-04 12:30:06 +0700

    Add native Android background music bridge

--- lib/services/background_music_stub.dart
+++ lib/services/background_music_stub.dart
@@ -1,17 +1,37 @@
 import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
+
+import '../core/services/app_settings.dart';
 
 class BackgroundMusic {
   BackgroundMusic._();
 
   static final BackgroundMusic instance = BackgroundMusic._();
 
-  final ValueNotifier<bool> enabled = ValueNotifier<bool>(false);
+  static const _channel = MethodChannel('irkop/background_music');
+  final ValueNotifier<bool> enabled = ValueNotifier<bool>(true);
 
-  void start() {}
+  Future<void> start() async {
+    if (!enabled.value) return;
+    try {
+      await _channel.invokeMethod('start', {
+        'volume': AppSettings.instance.musicVolume.value,
+      });
+    } catch (_) {}
+  }
 
-  void stop() {}
+  Future<void> stop() async {
+    try {
+      await _channel.invokeMethod('stop');
+    } catch (_) {}
+  }
 
-  void toggle() {
+  Future<void> toggle() async {
     enabled.value = !enabled.value;
+    if (enabled.value) {
+      await start();
+    } else {
+      await stop();
+    }
   }
 }
